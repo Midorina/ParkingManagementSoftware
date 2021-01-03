220721d58c8a3ae5527abf7c0ea970acb26eb1ce
Midoriin <fbyigit07@hotmail.com>
2021-01-03 17:27:51 +0300

    initial clean up and organizing

--- src/main/parking_lot/ParkingLot.java
+++ src/main/parking_lot/ParkingLot.java
@@ -1,8 +1,12 @@
 package main.parking_lot;
 
+import com.sun.javafx.geom.Rectangle;
+import javafx.application.Application;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 import main.vehicles.Vehicle;
 
-public class ParkingLot {
+public class ParkingLot extends Application {
     private ParkingLotFloor[] parkingLotFloors;
 
     public ParkingLot(int numberofLevels) {
@@ -11,21 +15,29 @@ public class ParkingLot {
         for (int i = 0; i < numberofLevels; i++) {
             parkingLotFloors[i] = new ParkingLotFloor(i);
         }
-    };
+    }
 
     public ParkingLot() {
         this(5);
-    };
+    }
 
     // park in a spot, return false if failed
     public boolean parkVehicle(Vehicle vehicle) {
         for (ParkingLotFloor lvl: this.parkingLotFloors) {
             ParkingSpot spot = lvl.getRandomAvailableSpot(vehicle);
+            // if an empty spot is found, park
             if (spot != null) {
                 vehicle.parkinSpot(spot);
                 return true;
             }
         }
         return false;
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        GridPane grid = new GridPane();
+
+        Rectangle parkingSpotObj = new Rectangle();
     }
 }
